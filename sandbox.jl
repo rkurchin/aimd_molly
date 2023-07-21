f9fe1da53add72af57323738721ce605511657b3
Rachel Kurchin <rkurchin@cmu.edu>
2023-07-21 12:41:17 -0400

    fix units, blank callback, wee bit of code golf
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- sandbox.jl
+++ sandbox.jl
@@ -12,26 +12,27 @@ function FlexibleSystem(c::Crystal)
 end
 
 # a few other convenient conversion-related functions
-positions(m::Model) = [m.lattice * pos .* u"Å" for pos in m.positions]
+# positions(m::Model) = [m.lattice * pos .* u"Å" for pos in m.positions]
 frac_coords(vec, lat) = [ustrip(dot(vec, el)/norm(el)^2) for el in lat]
 
 function get_frac_coords(coords, model::Model)
     lat = auconvert.(Ref(u"Å"), model.lattice) # DFTK is in Bohr!
-    lat_vecs = [lat[:, 1], lat[:, 2], lat[:, 3]]
-    frac_coords.(coords, Ref(lat_vecs))
+    frac_coords.(coords, Ref([lat[:, 1], lat[:, 2], lat[:, 3]]))
 end
 
 # getting forces from DFTK
-function get_forces(coords, dftk_model=dftk_model)
+function get_forces(coords, dftk_model; kgrid = [2, 2, 2], Ecut = 5)
+    println(Dates.now())
+
     # calculate fractional coordinates and update positions
-    dftk_model = Model(dftk_model, positions=get_frac_coords(coords, dftk_model))
+    new_model = Model(dftk_model, positions=get_frac_coords(coords, dftk_model))
 
     # build new basis
-    basis = PlaneWaveBasis(dftk_model; Ecut, kgrid)
+    basis = PlaneWaveBasis(new_model; Ecut, kgrid)
 
     # and calculate
-    scfres = self_consistent_field(basis, tol=1e-5);
-    [uconvert.(molly_sys.force_units, f .* u"Eh_au/bohr/mol") for f in compute_forces_cart(scfres)]
+    scfres = self_consistent_field(basis, tol=1e-5; callback=x->print()); # blank callback to print less stuff
+    compute_forces_cart(scfres) .* u"Eh_au/bohr"
 end
 
 # define a new Molly interaction using this
@@ -39,39 +40,30 @@ struct DFTKInter
     model::Model
 end
 
-function Molly.forces(inter::DFTKInter, sys, neighbors=nothing; n_threads=Threads.nthreads())
-    return get_forces(sys.coords, inter.model)
-end
+Molly.forces(inter::DFTKInter, sys, neighbors=nothing; n_threads=Threads.nthreads()) = get_forces(sys.coords)
 
 # build initial geometry
 silicon_crystal = Diamond(5.431u"Å", :Si, SVector{3}([1,1,1]))
 
 # set up initial DFTK stuff
-Si = ElementPsp(:Si, psp=load_psp("hgh/lda/Si-q4"))
-atoms = [Si for i in 1:8]
+atoms = [ElementPsp(:Si, psp=load_psp("hgh/lda/Si-q4")) for i in 1:length(silicon_crystal)]
 dftk_model = Model(model_LDA(FlexibleSystem(silicon_crystal)), 
                    atoms = atoms, 
                    temperature = 0.001, 
-                   smearing = DFTK.Smearing.FermiDirac(),
+                   smearing = DFTK.Smearing.FermiDirac(), 
                    symmetries = false)
-# pos = dftk_model.positions
-# pos[1] = pos[1] .+ rand(3) * 1e-3 
-# dftk_model = Model(dftk_model, positions=pos)
-kgrid = [2, 2, 2]
-Ecut = 5
 
 # set up Molly stuff
-temp = 10u"K"
-velocities = [random_velocity(atom.mass, temp) for atom in silicon_crystal.atoms]
-
 molly_sys = System(c, 
                    general_inters = (DFTKInter(dftk_model),),
-                   velocities = velocities,
-                   loggers = (coords=CoordinateLogger(10),))
+                   velocities = [random_velocity(atom.mass, 100u"K") for atom in silicon_crystal.atoms],
+                   force_units = u"Eh_au/bohr",
+                   loggers = (coords=CoordinateLogger(5),
+                              velocities=VelocityLogger(5),
+                              )
+                              )
 
-simulator = VelocityVerlet(dt=0.002u"ps",
-                        #    coupling=AndersenThermostat(temp, 1.0u"ps")
-                           )
+simulator = VelocityVerlet(dt=0.002u"ps")
 
-simulate!(molly_sys, simulator, 5)
+simulate!(molly_sys, simulator, 750)
 visualize(molly_sys.loggers.coords, molly_sys.boundary, "sim.mp4")
